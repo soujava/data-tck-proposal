44394dab5c675889ae8ebd0f2ff15764038bd36f
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-06 05:45:36 +0100

    feat: update book supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/br/org/soujava/jakarta/data/tck/BookSupplier.java
+++ tck/src/main/java/br/org/soujava/jakarta/data/tck/BookSupplier.java
@@ -1,5 +1,5 @@
 package br.org.soujava.jakarta.data.tck;
 
 public interface BookSupplier {
-    Book of(String isbn, String title, int edition);
+    Book apply(String isbn, String title, int edition);
 }
