65097aa4f5b94ae0feef9e18cce1d086b9556f8b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-06 05:56:18 +0100

    docs: update documentation at LibrarySupplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/br/org/soujava/jakarta/data/tck/LibrarySupplier.java
+++ tck/src/main/java/br/org/soujava/jakarta/data/tck/LibrarySupplier.java
@@ -1,6 +1,14 @@
 package br.org.soujava.jakarta.data.tck;
 
+import jakarta.data.repository.CrudRepository;
+
 import java.util.function.Supplier;
 
-public interface LibrarySupplier<Book, String> extends Supplier<> {
+/**
+ * A supplier of a {@link CrudRepository} implementation
+ * @param <B> the Book implementation where the vendor will give the annotations
+ * @param <T> the CrudRepository implementation
+ */
+public interface LibrarySupplier<B extends Book, T extends CrudRepository<B, String>>
+        extends Supplier<T> {
 }
