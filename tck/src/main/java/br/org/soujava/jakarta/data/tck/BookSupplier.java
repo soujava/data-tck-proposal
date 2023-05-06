3f16487d0ebe1e334c234f34c79224cc6a62b056
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-06 05:47:29 +0100

    docs: create documentation book supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/br/org/soujava/jakarta/data/tck/BookSupplier.java
+++ tck/src/main/java/br/org/soujava/jakarta/data/tck/BookSupplier.java
@@ -1,5 +1,16 @@
 package br.org.soujava.jakarta.data.tck;
 
+/**
+ * A supplier of Book.
+ * It will create a implementation of {@link Book} with the respective annotation.
+ */
 public interface BookSupplier {
+    /**
+     * Creates Book instance using the custom annotation
+     * @param isbn the isbn
+     * @param title the book's title
+     * @param edition the book's edition
+     * @return a book instance using vendor annotation
+     */
     Book apply(String isbn, String title, int edition);
 }
