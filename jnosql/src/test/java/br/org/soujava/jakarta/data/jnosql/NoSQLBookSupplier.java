11737614657bf82539462c949f7d459fd7566610
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-06 18:13:56 +0100

    feat: create nosql book supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql/src/test/java/br/org/soujava/jakarta/data/jnosql/NoSQLBookSupplier.java
@@ -0,0 +1,16 @@
+package br.org.soujava.jakarta.data.jnosql;
+
+import java.util.Objects;
+
+public class NoSQLBookSupplier implements BookSupplier {
+    @Override
+    public Book apply(String isbn, String title, String author, int edition) {
+        Objects.requireNonNull(isbn, "isbn is required");
+        Objects.requireNonNull(title, "title is required");
+        Objects.requireNonNull(author, "author is required");
+        if (edition < 0) {
+            throw new IllegalArgumentException("the edition cannot be negative");
+        }
+        return new NoSQLBook(isbn, title, edition, author);
+    }
+}
