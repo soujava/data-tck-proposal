9bdb83d5c938b315b161671a39fd8d0eefdf0fd3
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-06 18:34:38 +0100

    feat: create vendor tests
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql/src/test/java/br/org/soujava/jakarta/data/jnosql/VendorTest.java
@@ -0,0 +1,16 @@
+package br.org.soujava.jakarta.data.jnosql;
+
+import org.junit.jupiter.api.Test;
+
+public class VendorTest {
+
+    @Test
+    public void shouldUseImplementation(){
+        System.out.println("The implementation");
+    }
+
+    @Test
+    public void shouldAsVendor(){
+        System.out.println("As vendor you can do any test");
+    }
+}
