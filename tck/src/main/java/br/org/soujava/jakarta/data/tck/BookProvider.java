d3424ceae9f731188c42c01e4f4c0b3121ca2ef5
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-06 17:53:15 +0100

    feat: update docs on repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/br/org/soujava/jakarta/data/tck/BookProvider.java
+++ tck/src/main/java/br/org/soujava/jakarta/data/tck/BookProvider.java
@@ -7,23 +7,28 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
 import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
 
 public class BookProvider implements ArgumentsProvider {
 
-    private final Faker faker = new Faker();
+    private static final Faker FAKER = new Faker();
 
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext context)
             throws Exception {
 
-        Book book = faker.book();
+        br.org.soujava.jakarta.data.tck.Book entity = getBook();
+        return Stream.of(Arguments.of(entity,
+                LibrarySupplier.supplier()));
+    }
+
+    private static br.org.soujava.jakarta.data.tck.Book getBook() {
+        Book book = FAKER.book();
         BookSupplier supplier = BookSupplier.supplier();
         String isbn = UUID.randomUUID().toString();
         String title = book.title();
         String author = book.author();
-        return Stream.of(Arguments.of(supplier.apply(isbn, title, author, 1),
-                LibrarySupplier.supplier()));
+        br.org.soujava.jakarta.data.tck.Book entity = supplier.apply(isbn, title, author, 1);
+        return entity;
     }
 }
--- tck/src/main/java/br/org/soujava/jakarta/data/tck/CrudRepositoryTest.java
+++ tck/src/main/java/br/org/soujava/jakarta/data/tck/CrudRepositoryTest.java
@@ -1,4 +1,38 @@
 package br.org.soujava.jakarta.data.tck;
 
+
+import jakarta.data.repository.CrudRepository;
+import org.assertj.core.api.SoftAssertions;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicReference;
+
+@DisplayName("The scenario cover the basic methods to CrudRepository")
 public class CrudRepositoryTest {
+    private static final Duration DURATION = Duration.ofSeconds(5);
+
+    @ParameterizedTest
+    @ArgumentsSource(BookProvider.class)
+    @DisplayName("Should insert when the database is not there")
+    public void shouldSave(Book book, CrudRepository<Book, String> repository) {
+        repository.deleteAll();
+        repository.save(book);
+        AtomicReference<Book> reference = new AtomicReference<>();
+        //BASE
+        Awaitility.await().atMost(DURATION)
+                .until(() -> {
+                    repository.findById(book.isdn()).ifPresent(reference::set);
+                    return reference.get() != null;
+                });
+        Book updated = reference.get();
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(book.isdn()).isEqualTo(updated.isdn());
+            soft.assertThat(book.author()).isEqualTo(updated.author());
+            soft.assertThat(book.title()).isEqualTo(updated.title());
+        });
+    }
 }
