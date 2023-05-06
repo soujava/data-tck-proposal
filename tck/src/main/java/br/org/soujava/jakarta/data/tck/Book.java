0ed605f6d84df93c639d620354d4b36da93b1c4d
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-06 05:31:53 +0100

    feat: create Book entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tck/src/main/java/br/org/soujava/jakarta/data/tck/Book.java
@@ -0,0 +1,37 @@
+package br.org.soujava.jakarta.data.tck;
+
+/**
+ * A book is a medium for recording information in the form of writing or images,
+ * typically composed of many pages bound together and protected by a cover.
+ */
+public interface Book {
+
+    /**
+     * International Standard Book Number (ISBN) is a numeric
+     * commercial book identifier that is intended to be unique.
+     * @return the Book's id.
+     */
+    String isdn();
+
+    /**
+     * The Book's title
+     * @return The Book's title
+     */
+    String title();
+
+    /**
+     * The bibliographical definition of an edition is all copies of
+     * a book printed from substantially
+     * the same setting of type, including all minor typographical variants.
+     * @return the book's edition
+     */
+    int edition();
+
+    /**
+     * the date that books are shipped from a distribution
+     * center to online and retail stores
+     * @return the year of release
+     */
+    int release();
+
+}
