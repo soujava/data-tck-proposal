ec153d8295de088d75b48816787564c25199aac2
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-06 18:19:34 +0100

    chore: create manager supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql/src/test/java/br/org/soujava/jakarta/data/jnosql/ManagerSupplier.java
@@ -0,0 +1,28 @@
+package br.org.soujava.jakarta.data.jnosql;
+
+import jakarta.annotation.Priority;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.interceptor.Interceptor;
+import org.eclipse.jnosql.communication.document.DocumentManager;
+import org.eclipse.jnosql.databases.mongodb.communication.MongoDBDocumentManager;
+
+import java.util.function.Supplier;
+
+@ApplicationScoped
+@Alternative
+@Priority(Interceptor.Priority.APPLICATION)
+public class ManagerSupplier implements Supplier<DocumentManager> {
+
+    @Produces
+    public DocumentManager get() {
+        MongoDBDocumentManager manager = Container.INSTANCE.get("library");
+        return manager;
+    }
+
+    public void close(@Disposes DocumentManager manager) {
+        manager.close();
+    }
+}
