32a597959265e36ae3574c833a31853451846d57
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-06 18:11:44 +0100

    feat: create nosqlbook
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql/src/test/java/br/org/soujava/jakarta/data/jnosql/NoSQLBook.java
+++ jnosql/src/test/java/br/org/soujava/jakarta/data/jnosql/NoSQLBook.java
@@ -1,8 +1,32 @@
 package br.org.soujava.jakarta.data.jnosql;
 
 
+import jakarta.nosql.Column;
 import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.util.Objects;
 
 @Entity
-public class NoSQLBook {
+public record NoSQLBook(@Id String isbn,
+                        @Column("title") String title,
+                        @Column("edition") int edition,
+                        @Column("author") String author) implements Book {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        NoSQLBook noSQLBook = (NoSQLBook) o;
+        return Objects.equals(isbn, noSQLBook.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(isbn);
+    }
 }
