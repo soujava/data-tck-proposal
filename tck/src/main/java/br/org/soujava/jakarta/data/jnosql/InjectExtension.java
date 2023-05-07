561519361c3aee7408b5df963d03b83fa73ca783
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-07 06:55:45 +0100

    chore: define extension to inject
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tck/src/main/java/br/org/soujava/jakarta/data/jnosql/InjectExtension.java
+++ tck/src/main/java/br/org/soujava/jakarta/data/jnosql/InjectExtension.java
@@ -1,5 +1,7 @@
 package br.org.soujava.jakarta.data.jnosql;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -7,5 +9,6 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Target(TYPE)
 @Retention(RUNTIME)
+@ExtendWith(InjectJUnitExtension.class)
 public @interface InjectExtension {
 }
