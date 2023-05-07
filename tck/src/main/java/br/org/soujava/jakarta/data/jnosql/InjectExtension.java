0169fa67720ac043844c37ef38d298ae98ce3a3e
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-07 06:54:03 +0100

    chore: create extension
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tck/src/main/java/br/org/soujava/jakarta/data/jnosql/InjectExtension.java
@@ -0,0 +1,11 @@
+package br.org.soujava.jakarta.data.jnosql;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface InjectExtension {
+}
---
+++ tck/src/main/java/br/org/soujava/jakarta/data/jnosql/InjectJUnitExtension.java
@@ -0,0 +1,2 @@
+package br.org.soujava.jakarta.data.jnosql;public class InjectJUnitExtension {
+}
